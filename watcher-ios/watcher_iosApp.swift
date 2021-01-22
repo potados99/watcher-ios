12923290f4cdb54cdcc24e0fa709a180164e7b07
potados99 <potados99@gmail.com>
2021-01-22 22:39:47 +0900

    Initial Commit

---
+++ watcher-ios/watcher_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  watcher_iosApp.swift
+//  watcher-ios
+//
+//  Created by Potados on 2021/01/22.
+//
+
+import SwiftUI
+
+@main
+struct watcher_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
